a1fb07f9900160149628ddcf52e6af741e639ccf
naveenpremchand02 <bravoheart09@gmail.com>
2013-03-22 09:55:57 -0700

    Update settings.py
    last

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -116,7 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'blog/blogs',
+     'blogs',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
